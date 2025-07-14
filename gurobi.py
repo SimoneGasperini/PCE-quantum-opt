7162bdc20551917fb974082a35b9aba43abf3b99
Simone Gasperini <simone.gasperini4@unibo.it>
2025-07-14 16:56:12 -0400

    Apply suggestion

--- gurobi.py
+++ gurobi.py
@@ -12,17 +12,11 @@ class MaxCutGurobi(Base):
         self.vars = {}
         for i in self.graph.nodes:
             self.vars[i] = self.model.addVar(vtype=gp.GRB.BINARY)
-        self.obj = 0
-        for i, j in graph.edges:
-            w = graph[i][j].get("weight", 1)  
-            term = w * (self.vars[i] + self.vars[j] - 2 * self.vars[i] * self.vars[j])
-            self.obj += term
-        
-        # self.obj = sum(
-        #     graph[i][j].get("weight", 1) * (-self.vars[i] - self.vars[j] + 2 * self.vars[i] * self.vars[j])
-        #     for i, j in graph.edges
-        # )
-        self.model.setObjective(self.obj, sense=gp.GRB.MAXIMIZE)
+        obj = sum(
+            edge.get("weight", 1) * (1 - self.vars[i] * self.vars[j])
+            for i, j, edge in graph.edges(data=True)
+        )
+        self.model.setObjective(obj, sense=gp.GRB.MAXIMIZE)
 
     def run_model(self, verbose):
         if not verbose:
